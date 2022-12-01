2bf9690b40fba80019a0f357fdd472f83fdb577a
hkd92 <hassanekadridodo@gmail.com>
2022-12-01 21:11:30 +0100

    create csv file  for links

--- scrap.py
+++ scrap.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = "http://books.toscrape.com/index.html"
 req = requests.get(url)
@@ -7,12 +8,11 @@ req = requests.get(url)
 if req.ok:
     res = BeautifulSoup(req.text, "html.parser")
     liste = res.find(class_="nav nav-list")
-    lien = liste.findAll("a")
-    for link in lien:
-        print('http://books.toscrape.com/' + link.get('href'))
-    """"
-    arrayTest = []
-    for test in lien:
-        arrayTest.append(test)
-    print(arrayTest[1:])
-    """
+    links = liste.findAll("a")
+    arrayLinks = []
+    for link in links:
+        arrayLinks.append('http://books.toscrape.com/' + link.get('href'))
+    # print(arrayLinks)
+    with open("links.csv", 'w') as dataLinks:
+        writer = csv.writer(dataLinks, delimiter='\n')
+        writer.writerow(arrayLinks)
