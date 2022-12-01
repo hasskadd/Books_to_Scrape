a4a214ee14fe0a865d905cd0d124a109b90e1cea
hkd92 <hassanekadridodo@gmail.com>
2022-12-01 20:32:07 +0100

    categories links

--- scrap.py
+++ scrap.py
@@ -3,10 +3,16 @@ from bs4 import BeautifulSoup
 
 url = "http://books.toscrape.com/index.html"
 req = requests.get(url)
-res = BeautifulSoup(req.text, "html.parser")
-liste = res.find(class_="nav nav-list")
-lien = liste.findAll("a")
-arrayTest = []
-for test in lien:
-    arrayTest.append(test)
-print(arrayTest[1])
+
+if req.ok:
+    res = BeautifulSoup(req.text, "html.parser")
+    liste = res.find(class_="nav nav-list")
+    lien = liste.findAll("a")
+    for link in lien:
+        print('http://books.toscrape.com/' + link.get('href'))
+    """"
+    arrayTest = []
+    for test in lien:
+        arrayTest.append(test)
+    print(arrayTest[1:])
+    """
