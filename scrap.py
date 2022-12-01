07fdd52d368074671ff8ceba4c42b9b2e9b3e28f
hkd92 <hassanekadridodo@gmail.com>
2022-12-01 20:15:10 +0100

    array links

--- scrap.py
+++ scrap.py
@@ -4,5 +4,9 @@ from bs4 import BeautifulSoup
 url = "http://books.toscrape.com/index.html"
 req = requests.get(url)
 res = BeautifulSoup(req.text, "html.parser")
-
-print(res)
+liste = res.find(class_="nav nav-list")
+lien = liste.findAll("a")
+arrayTest = []
+for test in lien:
+    arrayTest.append(test)
+print(arrayTest[1])
