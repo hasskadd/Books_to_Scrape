81839367d517ab71651e51d6ff213fb15b4bdabb
hkd92 <hassanekadridodo@gmail.com>
2022-12-01 16:47:37 +0100

    install beautifulSoup and config

--- scrap.py
+++ scrap.py
@@ -3,5 +3,6 @@ from bs4 import BeautifulSoup
 
 url = "http://books.toscrape.com/index.html"
 req = requests.get(url)
-res = req.text
-print(res, 'html.parser')
+res = BeautifulSoup(req.text, "html.parser")
+
+print(res)
