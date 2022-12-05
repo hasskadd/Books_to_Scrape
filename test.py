f6cc52f81760681becddf57b840c0a649303ebef
hkd92 <hassanekadridodo@gmail.com>
2022-12-05 20:06:36 +0100

    download image and categorie csv files

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# read DataFrame
-data = pd.read_csv("products.csv")
-
-for (Category), group in data.groupby(['category']):
-    group.to_csv(f'categories/{Category}.csv', index=False)
